b32af0fc8decf2f1110307698a87a36840088519
Breyner794 <jhoanquilindo@gmail.com>
2025-06-20 18:12:40 -0500

    feat: Integracion de las nuevas pantallas para la reserva.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Faq from './pages/Faq';
+import { BookingProvider } from './context/BookingContext';
+import ServiceScreen from './screens/ServiceScreen';
+import SiteScreen from './screens/SiteScreen';
+import BarberScreen from './screens/BarberScreen';
+import DateTimeScreen from './screens/DateTimeScreen';
+import ConfirmationScreen from './screens/ConfirmationScreen';
+import CheckReservationPage from './pages/CheckReservationPage';
 
 function App() {
   return (
+    <BookingProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/Faq" element={<Faq />} />
+        <Route path="/consultar-reserva" element={<CheckReservationPage />} />
+        <Route path="/reservar" element={<ServiceScreen />} />
+        <Route path="/reservar/sede" element={<SiteScreen />} />
+        <Route path="/reservar/barbero" element={<BarberScreen />} />
+        <Route path='/reservar/fecha-hora' element={<DateTimeScreen/>}/>
+        <Route path='/reservar/confirmacion' element={<ConfirmationScreen/>}/>
       </Routes>
     </BrowserRouter>
+    </BookingProvider>
   );
 }
 
