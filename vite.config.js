f223b7f75171a43432430bbf447fecd01abb7c06
Breyner794 <jhoanquilindo@gmail.com>
2025-07-26 19:05:09 -0500

    configuracion para los puertos de deplegue

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173
+  }
 })
