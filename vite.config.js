ab2263a6a2a31cc762c31560f9176460b9345756
Breyner794 <jhoanquilindo@gmail.com>
2025-08-19 13:55:23 -0500

    fix: Correccion de los hosts

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
-    strictPort: true
+    strictPort: true,
+    allowedHosts: 'all'
   },
   
   build: {
